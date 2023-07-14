ca046daa2df90b602d98f52372cf50a7a98ccd18
Ronierison <ronierison.silva@k2m.com.br>
2023-07-13 22:06:17 -0300

    Criando a primeira classe

---
+++ CursoPOO/CursoPOO/CarrinhoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoPOO
+{
+    public class CarrinhoItem
+    {
+        //Estados da classe 
+
+        public string nome;
+        public int quantidade;
+        public decimal preco;
+
+    }
+}
--- CursoPOO/CursoPOO/Program.cs
+++ CursoPOO/CursoPOO/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CursoPOO;
+
+CarrinhoItem carrinhoItem = new CarrinhoItem();// criando um objeto de uma classe, nesse caso "carrinhoItem" é uma instancia da classe.
+carrinhoItem.nome = "Bola de futebol";
+carrinhoItem.quantidade = 1;
+carrinhoItem.preco = 99.00M;
+
